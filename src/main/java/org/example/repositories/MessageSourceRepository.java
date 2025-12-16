c374cbce20cbaec7277169308cba81b6c20bae49
Rosen Todorov <122826159+RosenTodorov2006@users.noreply.github.com>
2025-12-16 22:08:31 +0200

    Update repos names

--- src/main/java/org/example/repositories/MessageSourceRepository.java
+++ src/main/java/org/example/repositories/MessageSourceRepository.java
@@ -1,8 +1,9 @@
 package org.example.repositories;
 
+import org.example.models.entities.MessageSource;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MessageSource extends JpaRepository<MessageSource, Long> {
+public interface MessageSourceRepository extends JpaRepository<MessageSource, Long> {
 }
