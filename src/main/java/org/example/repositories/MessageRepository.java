5ac7d9721fe98e0f8780a3d10b82ffb03d63b7d6
Rosen Todorov <122826159+RosenTodorov2006@users.noreply.github.com>
2025-12-28 18:45:06 +0200

    Change base structure

--- src/main/java/org/example/repositories/MessageRepository.java
+++ src/main/java/org/example/repositories/MessageRepository.java
@@ -1,8 +1,9 @@
 package org.example.repositories;
 
+import org.example.models.entities.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Message extends JpaRepository<Message, Long> {
+public interface MessageRepository extends JpaRepository<Message, Long> {
 }
