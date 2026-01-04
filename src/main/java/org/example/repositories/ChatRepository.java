148da2ed258b125ff6d3bca69c0bfd10fb6a489d
Rosen Todorov <122826159+RosenTodorov2006@users.noreply.github.com>
2026-01-05 00:05:42 +0200

    Add dashboard page logic

--- src/main/java/org/example/repositories/ChatRepository.java
+++ src/main/java/org/example/repositories/ChatRepository.java
@@ -4,6 +4,10 @@ import org.example.models.entities.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
+    List<Chat> findAllByUserEntityId(Long userId);
+
 }
